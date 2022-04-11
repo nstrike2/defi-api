b033cbfb80b4f4c698a59916a07848611d6d21bb
Marcus Luebke <marcusxluebke@gmail.com>
2022-04-11 03:09:07 -0700

    Changed spaces to tabs

--- client/src/App.js
+++ client/src/App.js
@@ -4,41 +4,41 @@ import APISearchBox from "./components/APISearchBox";
 import  Adam from "./adam"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    Adam.initialize();
-  }
+	constructor(props) {
+		super(props);
+		Adam.initialize();
+	}
 
-  isWalletConnected = () => {
-    return false;
-  }
+	isWalletConnected = () => {
+		return false;
+	}
 
-  connectToMetaMask = async () => {
-    // All it takes to connect to metamask
-    Adam.connect();
-  };
+	connectToMetaMask = async () => {
+		// All it takes to connect to metamask
+		Adam.connect();
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p className="Text-header">Axon</p>
-          {this.isWalletConnected()
-            ? (<button className="Wallet-button">Wallet Connected</button>)
-            : (<button className="Wallet-button" onClick={this.connectToMetaMask}> Connect Wallet </button>)}
-        </header>
+	render() {
+		return (
+		<div className="App">
+			<header className="App-header">
+				<p className="Text-header">Axon</p>
+				{this.isWalletConnected()
+				? (<button className="Wallet-button">Wallet Connected</button>)
+				: (<button className="Wallet-button" onClick={this.connectToMetaMask}> Connect Wallet </button>)}
+			</header>
 
-        <div className="Main-container">
-          <div className="Text-title-big">Lend. Stake. Yield.</div>
-          <div className="Text-title-small">
-            Showcasing the world's most powerful universal DeFi API.
-          </div>
+			<div className="Main-container">
+				<div className="Text-title-big">Lend. Stake. Yield.</div>
+				<div className="Text-title-small">
+				Showcasing the world's most powerful universal DeFi API.
+				</div>
 
-         <APISearchBox/>
-        </div>
-      </div>
-    );
-  }
+			<APISearchBox/>
+			</div>
+		</div>
+		);
+	}
 }
 
 export default App;
--- client/src/adam.js
+++ client/src/adam.js
@@ -1,51 +1,51 @@
 import { ethers } from "ethers";
 
 export default class Adam {
-  static async initialize() {
-    await this._configureWallet();
-  }
+	static async initialize() {
+		await this._configureWallet();
+	}
 
-  static async connect() {
-    if (!this._hasWallet()) {
-      throw new Error("Wallet not found on browser.");
-    }
-    if (this.isWalletConnected()) {
-       // do stuff
-       console.log("Connected!");
-    } else {
-      await this._connectToWallet();
-      await this._configureWallet();
-    }
-  }
+	static async connect() {
+		if (!this._hasWallet()) {
+			throw new Error("Wallet not found on browser.");
+		}
+		if (this.isWalletConnected()) {
+			// do stuff
+			console.log("Connected!");
+		} else {
+			await this._connectToWallet();
+			await this._configureWallet();
+		}
+	}
 
-  static _configureWallet = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const accounts = await provider.listAccounts();
-    const signer =
-      accounts.length > 0 ? await provider.getSigner(accounts[0]) : null;
-    const network = await provider.getNetwork();
-    const signerAddress = signer != null ? await signer.getAddress() : null;
+	static _configureWallet = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const accounts = await provider.listAccounts();
+		const signer =
+			accounts.length > 0 ? await provider.getSigner(accounts[0]) : null;
+		const network = await provider.getNetwork();
+		const signerAddress = signer != null ? await signer.getAddress() : null;
 
-    this.provider = provider;
-    this.accounts = accounts;
-    this.signer = signer;
-    this.network = network;
-    this.signerAddress = signerAddress;
-  };
+		this.provider = provider;
+		this.accounts = accounts;
+		this.signer = signer;
+		this.network = network;
+		this.signerAddress = signerAddress;
+	};
 
-  static _hasWallet = () => {
-    return typeof window.ethereum !== "undefined" && window.ethereum !== null;
-  };
+	static _hasWallet = () => {
+		return typeof window.ethereum !== "undefined" && window.ethereum !== null;
+	};
 
-  static isWalletConnected = () => {
-    return this.signer != null;
-  };
+	static isWalletConnected = () => {
+		return this.signer != null;
+	};
 
-  static _connectToWallet = async () => {
-    await this.provider.send("eth_requestAccounts", []);
-  };
+	static _connectToWallet = async () => {
+		await this.provider.send("eth_requestAccounts", []);
+	};
 
-  static lend = () => {
-      
-  }
+	static lend = () => {
+			
+	}
 }
