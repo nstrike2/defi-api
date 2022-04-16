38cc571ea8e6139c80599f99302c42582145c001
Marcus Luebke <marcusxluebke@gmail.com>
2022-04-16 06:30:22 -0700

    Added sushiswap

--- routes/ethereum/mainnet/exchange/uniswap/index.js
+++ routes/ethereum/mainnet/exchange/uniswap/index.js
@@ -5,6 +5,12 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
+function toHex(data, props) {
+	for(let prop of props) {
+		data[prop] = parseInt(data[prop]).toString(16);
+	}
+}
+
 router.post("/swap", async (req, res) => {
 	try {
 		const { walletAddress, amount, buyToken, sellToken, gasPriority } = req.body;
@@ -19,6 +25,10 @@ router.post("/swap", async (req, res) => {
 				`https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`
 			);
 			const data = Object.assign({walletAddress}, response.data);
+			const toHexProps = ["sellAmount", "buyAmount", "estimatedGas", "gas", "gasPrice", "value"];
+			for(let prop of toHexProps) {
+				data[prop] = parseInt(data[prop]).toString(16);
+			}
 			res.json(data);
 		} else {
 			res.status(400).send("Invalid sell token");
