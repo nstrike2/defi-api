9c77531d0ba42d9ce48a12629de17f29ab875839
Neetish Sharma <neetishs@stanford.edu>
2022-04-20 01:51:26 -0700

    Adding UI final touches
    Adding UI final touches

--- client/src/App.js
+++ client/src/App.js
@@ -16,35 +16,44 @@ class App extends React.Component {
 
 	async connectToMetaMask() {
 		// All it takes to connect to metamask
-		if(await Adam.connect()) {
-			this.setState({isWalletConnected: Adam.isWalletConnected()});
+		if (await Adam.connect()) {
+			this.setState({ isWalletConnected: Adam.isWalletConnected() });
 		}
 	}
 
 	async componentDidMount() {
 		await Adam.initialize();
-		this.setState({isWalletConnected: Adam.isWalletConnected()});
+		this.setState({ isWalletConnected: Adam.isWalletConnected() });
 	}
 
 	render() {
 		return (
-		<div className="App">
-			<header className="App-header">
-				<p className="Text-header">Axel</p>
-				{this.state.isWalletConnected
-				? (<button className="Wallet-button Green-border">Wallet Connected</button>)
-				: (<button className="Wallet-button" onClick={this.connectToMetaMask}> Connect Wallet </button>)}
-			</header>
+			<div className="App">
+				<header class="navbar">
+					<span id="navbar-items">
+						<img
+							className="navbar-item Title-img"
+							src="axel-logo.svg"
+							alt="Axel Logo"
+						/>
+						<div className="navbar-item Text-header">axel</div>
+					</span>
+					<li id="wallet-button" className="navbar-item">
+						{this.state.isWalletConnected
+							? (<button className="Wallet-button Green-border">Wallet Connected</button>)
+							: (<button className="Wallet-button" onClick={this.connectToMetaMask}> Connect Wallet </button>)}
+					</li>
+				</header>
 
-			<div className="Main-container" >
-				<Title text = "Lend. Stake. Yield."/>
-				<div className="Text-title-small">
-				Showcasing the world's most powerful universal DeFi API.
+				<div className="Main-container" >
+					<Title text="Lend. Stake. Yield. Exchange." />
+					<div className="Text-title-small">
+						Showcasing the world's most powerful, composable, and universal DeFi developer platform.
+					</div>
+
+					<APISearchBox />
 				</div>
-
-			<APISearchBox/>
 			</div>
-		</div>
 		);
 	}
 }
--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -4,62 +4,44 @@ import "../App.css";
 class Title extends React.Component {
 	static endDelay = 1000;
 	static startDelay = 200;
-	static minorTypeDelay = 100;
+	static minorTypeDelay = 50;
 	static spaceTypeDelay = 20;
-	static majorTypeDelay = 400;
+	static majorTypeDelay = 200;
 	static deleteDelay = 50;
-	static initDelay = 3000;
+	static initDelay = 200;
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			forward: false,
-			text: this.props.text,
-			index: this.props.text.length,
-			isTyping: false,
+			text: "",
+			index: 0,
 		};
-		this.isTyping = false;
 	}
-	type(time = 0) {
+	type(time=0) {
 		setTimeout(() => {
-			if (this.state.forward) {
-				if (this.state.index == this.props.text.length) {
-					this.setState({
-						forward: false
-					});
-					this.type(Title.endDelay);
-				} else {
-					const char = this.props.text[this.state.index];
-					this.setState({
-						text: this.state.text + char,
-						index: this.state.index + 1,
-					});
-					this.type(char == "." ? Title.majorTypeDelay : Title.minorTypeDelay);
+			if (this.state.index !== this.props.text.length) {
+				const char = this.props.text[this.state.index];
+				this.setState({
+					text: this.state.text + char,
+					index: this.state.index + 1,
+				});
+				if (this.state.index > 0) {
+					this.appear = true;
 				}
-			} else {
-				if (this.state.index == 0) {
-					this.setState({
-						forward: true
-					});
-					this.type(Title.startDelay);
-				} else {
-					const char = this.props.text[this.state.index - 1];
-					this.setState({
-						text: this.state.text.slice(0, -1),
-						index: this.state.index - 1,
-					});
-					this.type(char == " " ? Title.spaceTypeDelay : Title.deleteDelay);
-				}
+				this.type(char === "." ? Title.majorTypeDelay : Title.minorTypeDelay);
 			}
 		}, time);
 	}
 	render() {
 		if(!this.isTyping) {
 			this.isTyping = true;
+			this.appear = false;
 			this.type(Title.initDelay);
 		}
 		return (
-			<div className = "Text-title-big">{ this.state.text }</div>
+			<div className = "Text-title-big">
+				{ (this.appear) ? this.state.text : <span>&nbsp;&nbsp;</span> }
+			</div>
 		);
 	}
 }
