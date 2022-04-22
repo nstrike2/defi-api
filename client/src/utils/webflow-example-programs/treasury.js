0429306d622c0ceecb1d2792753803f39e97108d
Marcus Luebke <marcusxluebke@gmail.com>
2022-04-22 00:42:58 -0700

    Sample treasury code

--- client/src/utils/webflow-example-programs/treasury.js
+++ client/src/utils/webflow-example-programs/treasury.js
@@ -1,7 +1,16 @@
 import Axel from "Axel.js";
-const apiKey = "861302973";
-const axel = Axel(`http://axelplatform.xyz/${apiKey}`);
+const { apiKey, routingNumber, accountNumber } = process.env;
+
+const axel = Axel.initialize(apiKey);
 await axel.start();
-await axel.create.lendingpool("Lido", {
-	amount: 0.08 // ETH
+
+const DeFiAccount = await axel.treasury.createDeFiAccount();
+const fiatBank = await axel.treasury.loadFiatAccount({routingNumber, accountNumber});
+await axel.treasury.fiatOnRamp(fiatBank, DeFiAccount, {
+	"token": "USDC",
+	"amount": 10000,
+});
+await DeFiAccount.lend("Rari", {
+	"amount": 1000,
+	"sellToken": "USDC"
 });
